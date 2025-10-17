a667619ece60b519eaabecdbf5e5047adf2d03a7
Sattarzade-Kanan <chinkan1980@gmail.com>
2025-10-17 19:42:43 +0400

    m

---
+++ BookMyNotFriends/src/test/java/com/example/BookMyNotFriends/BookMyNotFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookMyNotFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyNotFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
